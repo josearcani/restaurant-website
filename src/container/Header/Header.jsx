b3ca9315f1754b65c0562ec255bd6d5c5bf248c4
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-30 23:39:07 -0400

    ui: apply styles to navbar section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>asd asdf asd fas df asdf asd fa sdf as df as df asd fa sdf asdf asdf asdf asdf asdf asdf asdf asd</p>
+      <button type="button" className="custom__buttom">Explore Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={ images.welcome } alt="header img" />
+    </div>
   </div>
 );
 
